28ad9bcc3e4f051997fcda4701150a365f998d23
Hamza Ahmad <bscs24037@itu.edu.pk>
2025-06-12 16:13:01 +0500

    Commit 8

---
+++ Building.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Building.h"
+using namespace std;
+
+Building::Building() {
+    x = 0;
+    y = 0;
+    s_points = 0;
+}
+
+Building::Building(int a, int b, int s) {
+    x = a;
+    y = b;
+    s_points = s;
+}
+
+int Building::x_pos() {
+    return this->x;
+}
+
+int Building::y_pos() {
+    return this->y;
+}
+
+int Building::getShield() {
+    return this->s_points;
+}
+
+void Building::display() {
+    cout << "Entered Building! Received " << s_points << " shield points." << endl;
+}
