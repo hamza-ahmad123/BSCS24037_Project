cc55132aeaa6297ae1ede5eb57b7284401f7404d
Hamza Ahmad <bscs24037@itu.edu.pk>
2025-06-12 13:48:22 +0500

    Commit 5

---
+++ Enemy.h
@@ -0,0 +1,26 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "Robot.h"
+#include "Building.h"
+#include "HealingPoint.h"
+
+class Enemy : public Robot {
+private:
+    int damage;
+
+public:
+    Enemy();
+
+    Enemy(int a, int b, int h, int d);
+
+    void moveToward(int target_x, int target_y, Building* buildings, int b_count, HealingPoint* hps, int h_count);
+
+    bool inRange(int target_x, int target_y);
+
+    int getDamage();
+
+    void display() override;
+};
+
+#endif
