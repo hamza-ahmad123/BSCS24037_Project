466c40515a431aecbb1e285782a17fd38d792a5c
Hamza Ahmad <bscs24037@itu.edu.pk>
2025-06-12 13:56:29 +0500

    Commit 6

---
+++ Enemy.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Enemy.h"
+#include "Building.h"
+#include "HealingPoint.h"
+using namespace std;
+
+Enemy::Enemy() {
+    x = 0;
+    y = 0;
+    health = 50;
+    damage = 10;
+}
+
+Enemy::Enemy(int a, int b, int h, int d) {
+    x = a;
+    y = b;
+    health = h;
+    damage = d;
+}
+
+void Enemy::moveToward(int target_x, int target_y, Building* buildings, int b_count, HealingPoint* hps, int h_count) {
+    int _x = x;
+    int _y = y;
+
+    if (x < target_x) {
+        _x++;
+    }
+    else if (x > target_x) {
+        _x--;
+    }
+
+    if (y < target_y) {
+        _y++;
+    }
+    else if (y > target_y) {
+        _y--;
+    }
+
+    for (int i = 0; i < b_count; i++) {
+        if (buildings[i].x_pos() == _x && buildings[i].y_pos() == _y) {
+            return;
+        }
+    }
+
+    for (int i = 0; i < h_count; i++) {
+        if (hps[i].x_pos() == _x && hps[i].y_pos() == _y) {
+            return;
+        }
+    }
+
+    x = _x;
+    y = _y;
+}
+
+bool Enemy::inRange(int target_x, int target_y) {
+    int dx = x - target_x;
+    int dy = y - target_y;
+    return (dx * dx + dy * dy <= 1);
+}
+
+int Enemy::getDamage() {
+
+    return damage;
+}
+
+void Enemy::display() {
+    cout << "Enemy at (" << x << ", " << y << ") , ";
+    cout << "  Health: " << health << " , Damage: " << damage << endl;
+}
